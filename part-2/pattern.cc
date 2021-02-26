cce66ba8997a46a03b3bc45519dfc08574a2966d
Raxel Ortiz <raxelortiz7@csu.fullerton.edu>
2021-02-25 19:02:55 -0800

    I finished the code. I was having trouble figuring out what variables went in the inner loops but I got some help and figured it out. All i had to do was set int dashes and int lines equal to i, which makes sense because that is the what is increasing the number of our rows. The inner loops are both different. The first one represents dashes and how they increase from zero and the second inner loop shows how lines decrease as the number of rows increases

--- part-2/pattern.cc
+++ part-2/pattern.cc
@@ -1,25 +1,44 @@
+// Raxel Ortiz
+// CPSC 120-05
+// 2021-24-02
+// raxelortiz7@csu.fullerton.edu
+// @raxelortiz7
+//
+// Lab 03-02
+//
+// This program has an outer loop and two inner loops that produce a pattern when ran in the termial 
+//
 
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  // TODO: Declare and initialize a const integer named counter max (max for
+  // Declare and initialize a const integer named counter max (max for
   // maximum). Initialize it to 22.
-
-  // TODO: Write an outer loop which starts from 0 and goes up to the counter
+const int kCounterMax = 22;
+  // Write an outer loop which starts from 0 and goes up to the counter
   // max.
-
-  // TODO: Write an inner loop which starts from the current line number and
+for ( int i = 0; i < kCounterMax; i++ ) {
+  // Write an inner loop which starts from the current line number and
   // counts down to zero. Make sure that this loop is inside the outer loop.
 
-  // TODO: Print an asterisk.
+  string dash = "-";
+  for ( int dashes = i; dashes > 0; --dashes ) {
+    cout << dash << "";
+}
+  // Print an asterisk.
+  cout << "*";
 
-  // TODO: Write another inner loop which starts from the current line number
+  // Write another inner loop which starts from the current line number
   // and counts up to the counter max. Make sure that this loop is inside the
   // outer loop but outside the first inner loop.
-
-  // TODO: print a new line character
-
+  string lines = "|";
+  for ( int line = i; line < kCounterMax; ++line ) {
+    cout << lines << "";
+  }
+  // print a new line character
+cout << "\n";
+}
   return 0;
 }
